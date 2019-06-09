a74e060227a9e80c88862c34bea68026ac6368ce
crisBoi <pranaycris@gmail.com>
2019-06-09 17:17:52 +0530

    version 1.0

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -6,8 +6,19 @@ public class Hacker : MonoBehaviour
 {
 
 	private enum Screen { MainMenu, Password, Win};
+	
 	private Screen currentScreen = Screen.MainMenu;
+	
+	private string password = "";
+	private string[] level1Passwords = {"table", "apple", "boy", "shop", "green"};
+	private string[] level2Passwords = {"virus", "serum", "frog", "blood", "cell"};
+	private string[] level3Passwords = {"conscious", "saturn", "galaxy", "quantum", "particles"};
+	private string[] level1Hints = {"needed with a chair", "first fruit", "opposite of girl", "where shopkeeper works", "grass is"};
+	private string[] level2Hints = {"infected by", "serum", "first disection", "type group", "you are mad up of"};
+	private string[] level3Hints = {"only humans possess", "rings in solar system", "galaxy", "stream of physics to complicated to understand", "light is a"};
 
+
+	private int level, index = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -44,38 +55,193 @@ public class Hacker : MonoBehaviour
 	    else if (currentScreen == Screen.MainMenu) {
     		
 	    	RunMainMenu(input);
-    	} 
-    }
 
-
-    private void RunMainMenu(string input) {
-    	if (input == "1") {
-    		
-	    	StartGame(input);
-    		
-
-    	} else if (input == "2") {
-
-    		StartGame(input);
-    		
-
-    	} else if (input == "3") {
-    		
-    		StartGame(input);
-    	
-    		
-    	} else {
-    		showMainMenu();
-    		Terminal.WriteLine("Enter a valid number");
+    	} else if (currentScreen == Screen.Password) {
+    		checkPassword(input);
+    	} else if (currentScreen == Screen.Win) {
+    		RunMainMenu(input);
     	}
     }
 
 
-    private void StartGame(string level) {
+	 private void RunMainMenu(string input) {
+
+	    	
+	    	
+	    	bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+	    	
+	    	if (isValidLevelNumber) {
+	    		
+	    		level = int.Parse(input);
+	    		StartGame();
+
+	    	} else {
+	    		
+	    		showMainMenu();
+	    		Terminal.WriteLine("Enter a valid number");
+
+	    	}
+	    }
+
+
+    
+
+
+    private void StartGame() {
+    	
     	Terminal.ClearScreen();
-    	Terminal.WriteLine("You have entered " + level);
+    	Terminal.WriteLine("Welcome to level " + level);
     	Terminal.WriteLine("Enter your password :" );
 
     	currentScreen = Screen.Password;
+
+    	
+    	switch(level) {
+
+    		case 1 :
+    		
+    		SetLevel(1);
+    		
+
+    		break;
+
+    		case 2 : 
+
+    		SetLevel(2);
+    		
+    		
+    		break;
+
+    		case 3 : 
+    		
+    		SetLevel(3);
+    		
+    		
+    		break;
+
+    		default : Debug.LogError("Invalid level number");
+    		break;
+
+    	}
+
     }
+
+
+    private void checkPassword(string enteredPassword) {
+
+    	if (password == enteredPassword) {
+    		
+    		
+    		ShowReward();
+    		currentScreen = Screen.Win;
+    	
+    	} else {
+    		
+    		Terminal.WriteLine("Wrong password");
+    		Terminal.WriteLine("Enter your password :" );
+    		
+    		if (index < 4) {index++;}
+    		else {index = 0;}
+    		
+    		SetLevel(level);
+    		
+    	}
+
+    	
+    }
+   
+
+    private void ShowHint() {
+
+    	switch(level) {
+
+    		case 1 :
+    		Terminal.WriteLine("hint: " + level1Hints[index]);
+    		break;
+
+    		case 2 : Terminal.WriteLine("hint: " + level2Hints[index]);
+    		break;
+
+    		case 3 : Terminal.WriteLine("hint: " + level3Hints[index]);
+    		break;
+    	}
+    }
+
+
+
+    private void SetLevel(int currentLevel) {
+
+
+    	if (currentLevel == 1) {
+
+    		level = 1;
+    		ShowHint();
+    		password = level1Passwords[index];
+
+    	} else if (currentLevel == 2) {
+				
+			level = 2;
+			ShowHint();
+			password = level2Passwords[index];
+    		
+		} else if (currentLevel == 3) {
+			level = 3;
+			ShowHint();
+			password = level3Passwords[index];
+		}
+
+    	
+    }
+
+
+    private void ShowReward() {
+
+    	Terminal.ClearScreen();
+
+    	switch(level) {
+    		case 1 :
+    		Terminal.WriteLine("CONGRATULATIONS YOU HAVE BROKED INTO LIBRARY"); 
+    		Terminal.WriteLine(@"
+    -------
+   /      //
+  /      //
+ /______//
+(______(/");
+    		break;
+
+
+    		case 2 :
+    		Terminal.WriteLine("CONGRATULATIONS YOU HAVE BROKED INTO BIO CHEMIST LAB"); 
+    		Terminal.WriteLine(@"
+        
+  `MMMMMM/   /   \   _   ,    
+   MMM|  __  / __/  ( |_|
+   YMM/_/# \__/# \    | |_)arry
+   (.   \__/  \__/     ___  
+     )       _,  |    '_|_)
+_____/\     _   /       | otter
+    \  `._____,'
+     `..___(__");
+    		break;
+
+
+
+    		case 3 :
+    		Terminal.WriteLine("CONGRATULATIONS YOU HAVE BROKED INTO NASA"); 
+    		Terminal.WriteLine(@"
+   |||||
+ ||. .||
+|||\=/|||
+|.-- --.|
+/(.) (.)\
+\ ) . ( /
+'(  v  )`
+  \ | /
+  ( | )
+  '- -`");
+    		break;
+    	}
+    }
+	
+   
 }
